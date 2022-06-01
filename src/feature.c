e682418fe1f8af6cf1d66ed93da5263b2a125215
Elodie Bouzekri <ebouzekri@gmail.com>
2022-06-01 14:40:44 +0200

    correct typo

--- src/feature.c
+++ src/feature.c
@@ -5,10 +5,10 @@
 #include "utils.h"
 
 /**
- * @brief Here, you have to code features of project.
+ * @brief Here, you have to code features of the project.
  * Do not forget to commit regurlarly your changes.
  * Your commit messages must contain "#n" with: n = number of the corresponding feature issue.
- * When the feature is totally implement, your commit message must contain "close #n".
+ * When the feature is totally implemented, your commit message must contain "close #n".
  */
 
 void helloWorld() {
